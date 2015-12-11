65397f787a86116757db915d71386521e7883f99
Antoine Racine <racine.antoine@gmail.com>
2015-12-11 02:20:02 +0000

    Scafold python project using Yeoman

---
+++ workspace/test/workspace_test.py
@@ -0,0 +1,14 @@
+from unittest import TestCase, skip
+import workspace
+
+
+class workspaceTest(TestCase):
+	def setUp(self):
+		pass
+
+	def tearDown(self):
+		pass
+
+	@skip('Tests not implemented')
+	def test_something(self):
+		self.assertTrue(False)
