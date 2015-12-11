3059e197cff5ae8147a79cfb94ee3e7dd579ed21
Antoine Racine <racine.antoine@gmail.com>
2015-12-11 03:02:00 +0000

    Add two basic choice for the main command -> build or show

--- workspace/dnd.py
+++ workspace/dnd.py
@@ -2,7 +2,7 @@ import argparse
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument("command", help="main command used by the tool")
+parser.add_argument("command", help="main command used by the tool", choices=["build", "show"])
 parser.add_argument("-v", "--verbose", help="increase output verbosity",
                     action="store_true")
 args = parser.parse_args()
