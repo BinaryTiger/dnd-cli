0e54144c5caa446c1660cbed83d72e6f0c157f5a
Antoine Racine <racine.antoine@gmail.com>
2015-12-12 18:46:45 +0000

    Add basic structure for saving a mock city and showing a city by name

--- workspace/dnd.py
+++ workspace/dnd.py
@@ -1,19 +1,21 @@
 import argparse
+import json
 
 parser = argparse.ArgumentParser(prog='dnd')
 
 #Constants
 VERSION = "0.0.1"
 OUTPUT_PATH_FOR_TESTING = "../output/"
-CITY_FOLDER = "../output/city"
-NPC_FOLDER = "../output/npc"
-MOCK_CITY = "{name: 'MOCK CITY', population: 10000}"
-MOCK_NPC = "{name: 'MOCK NPC', age: 19}"
+CITY_FOLDER = "../output/city/"
+NPC_FOLDER = "../output/npc/"
+MOCK_CITY = {"name": "MOCK CITY", "population": 10000}
+MOCK_NPC = {"name": "MOCK NPC", "age": 19}
 
 
 #Arguments
 parser.add_argument("command", help="main command used by the tool", choices=["build", "show"])
 parser.add_argument("object", help="specify which kind of object the tool have to manipulate", choices=["city", "npc"])
+parser.add_argument("name", help="specify the name of the object")
 
 #Flags
 
@@ -46,16 +48,16 @@ if args.overwrite:
 if args.command == "build":
     print("We are building a " + args.object)
     
-    if isRandom:
-        print("Building a random " + args.object)
+    filepath = CITY_FOLDER + args.name + ".json"
+    with open(filepath, 'w') as outfile:
+        json.dump(MOCK_NPC, outfile, indent=4, separators=(',', ': '))
     
-    if isVerbose:
-        print("We are creating it verbosely")
     
 elif args.command == "show":
-    print("We are showing a " + args.object)
-    
-    if isRandom:
-        print("Showing a random " + args.object)
-    if isVerbose:
-        print("We are showing it verbosely")
+    if args.object == "city":
+        file_to_read = CITY_FOLDER + args.name + ".json"
+        print(file_to_read)
+        
+        with open(file_to_read, 'r') as infile:
+            data = json.load(infile)
+            print(json.dumps(data, indent=4, separators=(',', ': ')))
