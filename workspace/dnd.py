ec113a75b02d240c6c8cd2f927b46026c44a75f9
Antoine Racine <racine.antoine@gmail.com>
2015-12-12 16:24:37 +0000

    Add version flag

--- workspace/dnd.py
+++ workspace/dnd.py
@@ -1,10 +1,18 @@
 import argparse
 
-parser = argparse.ArgumentParser()
+parser = argparse.ArgumentParser(prog='dnd')
 
+#Constants
+VERSION = "0.0.1"
+
+
+#Arguments
 parser.add_argument("command", help="main command used by the tool", choices=["build", "show"])
+
+#Flags
 parser.add_argument("-v", "--verbose", help="increase output verbosity",
                     action="store_true")
+parser.add_argument("-V", "--version", action='version', version="%(prog)s " + VERSION)
 args = parser.parse_args()
 
 if args.verbose:
