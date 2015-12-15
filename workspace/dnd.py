2057f67efa8532854672feca6038e42d12ddabe1
Antoine Racine <racine.antoine@gmail.com>
2015-12-14 22:05:49 -0500

    Change indentation to spaces

--- workspace/dnd.py
+++ workspace/dnd.py
@@ -4,19 +4,19 @@ from city import City
 
 parser = argparse.ArgumentParser(prog='dnd')
 
-#Constants
+# Constants
 VERSION = "0.0.1"
 OUTPUT_PATH_FOR_TESTING = "../output/"
 MOCK_CITY = {"name": "MOCK CITY2", "population": 10000}
 MOCK_NPC = {"name": "MOCK NPC", "age": 19}
 
 
-#Arguments
+# Arguments
 parser.add_argument("command", help="main command used by the tool", choices=["build", "show"])
 parser.add_argument("object", help="specify which kind of object the tool have to manipulate", choices=["city", "npc"])
 parser.add_argument("name", help="specify the name of the object")
 
-#Flags
+# Flags
 parser.add_argument("--overwrite", help="ignores the file exist exception and overwrite it",
                     action="store_true")
 parser.add_argument("--random", help="ignores parameters and generate the object randomly",
@@ -33,7 +33,7 @@ isRandom = False
 isOverwrite = False;
 filepath = OUTPUT_PATH_FOR_TESTING + args.object + "/" + args.name + ".json"
 
-#Maybe I can loop the parameters and set the conresponding flags accordingly
+# Maybe I can loop the parameters and set the conresponding flags accordingly
 if args.verbose:
     isVerbose = True
 
@@ -42,18 +42,18 @@ if args.random:
 
 if args.overwrite:
     isOverwrite = True
-#End of flags settings
+# End of flags settings
 
 if args.command == "build":
     print("We are building a " + args.object)
-    
+
     if args.object == "city":
         city = City()
         city.name = args.name
         city.population = 10000
-        
+
         city.save_to_file(filepath);
-        
+
 elif args.command == "show":
     if args.object == "city":
         with open(filepath, 'r') as infile:
